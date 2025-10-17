ac071de064af03a33398e8cc9b1b625833dba25b
pankaspe <mez.tnt@gmail.com>
2025-10-17 13:57:50 +0200

    aggiunta animazione al testo

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -42,9 +42,7 @@ pub fn Navbar() -> impl IntoView {
                 <div class="navbar__right">
                     <A href="/login" attr:class="navbar__login-btn">
                         <span class="navbar__login-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
-                            </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-in-icon lucide-log-in"><path d="m10 17 5-5-5-5"/><path d="M15 12H3"/><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/></svg>
                         </span>
                     </A>
                 </div>
